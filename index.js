477e47c739e3ebb97857ac0349aeee8f816c0ac9
ninaschulte <>
2023-07-31 09:05:00 +0000

    Add quiz data to the js file

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-let welcomeHeader = document.getElementById("welcomeheader");
-
-welcomeHeader.innerHTML = "Welcome!";
+const historicQuestions = [
+    {
+        question: "Which famous scientist developed the theory of general relativity?",
+        options: ["Isaac Newton", "Galileo Galilei", "Albert Einstein", "Johannes Kepler"],
+        correctAnswer: 2
+    },
+    {
+        question: "The Declaration of Independence was adopted on which date?",
+        options: ["July 4, 1776", "July 2, 1776", "July 4, 1789", "July 2, 1789"],
+        correctAnswer: 0
+    },
+    {
+        question: "The Great Wall of China was primarily built to protect against invasions from which group?",
+        options: ["Mongols", "Vikings", "Romans", "Huns"],
+        correctAnswer: 3
+    },
+    {
+        question: "Who was the first President of the United States?",
+        options: ["Thomas Jefferson", "George Washington", "Benjamin Franklin", "Alexander Hamilton"],
+        correctAnswer: 1
+    },
+    {
+        question: "The French Revolution began in which year?",
+        options: ["1789", "1776", "1792", "1800"],
+        correctAnswer: 0
+    },
+    {
+        question: "Who was the Egyptian pharaoh who built the Great Pyramid of Giza?",
+        options: ["Tutankhamun", "Cleopatra", "Ramses II", "Khufu"],
+        correctAnswer: 3
+    },
+    {
+        question: "The Battle of Waterloo took place during which war?",
+        options: ["American Revolutionary War", "Napoleonic Wars", "English Civil War", "Hundred Years' War"],
+        correctAnswer: 1
+    },
+    {
+        question: "The Magna Carta, a key historical document, was signed in which year?",
+        options: ["1215", "1492", "1776", "1804"],
+        correctAnswer: 0
+    },
+    {
+        question: "Who was the first woman to fly solo across the Atlantic Ocean?", options: ["Amelia Earhart", "Rosa Parks", "Marie Curie", "Jane Austen"],
+        correctAnswer: 0
+    },
+    {
+        question: "In which year did the Berlin Wall fall, leading to the reunification of Germany?",
+        options: ["1989", "1975", "1991", "1985"],
+        correctAnswer: 0
+    }];
